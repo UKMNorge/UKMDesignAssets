4a8d5cd7eac9d8cb29682d1f625c4c2d6032fa39
Sigurd Sæther Sørensen <32162632+Sigurdssorensen@users.noreply.github.com>
2021-02-02 17:41:27 +0100

    Setup DSP & core name conventions

---
+++ UKM-Design_System-Package/dist/styledictionary/js/tokens 2.js
@@ -0,0 +1,25 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 02 Feb 2021 13:06:57 GMT
+ */
+
+export const ColorNeutralColdgray900 = "#080b14";
+export const ColorNeutralColdgray100 = "#f5f7fc";
+export const ColorNeutralColdgray400 = "#cad1e0";
+export const ColorNeutralColdgray700 = "#596580";
+export const ColorPrimaryOrange700 = "#cf6334";
+export const ColorPrimaryOrange800 = "#823514";
+export const ColorPrimaryOrange900 = "#230e0b";
+export const ColorPrimaryOrange400 = "#fac7b0";
+export const ColorPrimaryOrange500 = "#ff6920";
+export const ColorPrimaryOrange600 = "#f35623";
+export const ColorPrimaryOrange200 = "#faf2ed";
+export const ColorPrimaryOrange300 = "#fae9e1";
+export const ColorPrimaryOrange50 = "#fffefc";
+export const ColorPrimaryBlue600 = "#5879fc";
+export const ColorPrimaryBlue800 = "#2f55ed";
+export const ColorPrimaryBlue900 = "#02024a";
+export const ColorPrimaryBlue200 = "#c2d4fc";
+export const ColorPrimaryBlue400 = "#86a3fc";
+export const ColorPrimaryBlue100 = "#e3ecfc";
+export const ColorPrimaryBlue50 = "#f5f7fa";
