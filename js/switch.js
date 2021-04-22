ac0e532cad73f9bfa6035f59fbce1740ad3c6fcb
Kim Duong <kimd9740@gmail.com>
2021-04-22 09:19:20 +0200

    scss-files

--- js/switch.js
+++ js/switch.js
@@ -2,10 +2,15 @@ var text;
 
 jQuery(document).on('mouseover', '.testimonial-small', function(e) {
     jQuery('#testimonial .side-pic').html( jQuery(this).data('img'));
+
+    $('#testimonial .signature-text').animate({left: '250px'}, function(){
+        jQuery('#testimonial .signature-text').html( jQuery(this).data('text') );
+        jQuery('#testimonial .signature').html( jQuery(this).data('signature'));
+    }).delay(1000).fadeTo('slow', 1);
+
     if (text != jQuery(this).data('text')) {
         jQuery(this).css({opacity : "1"});
         jQuery('.testimonial-small:not(:hover)').css({opacity : "0.5"});
-
     }
     text = jQuery(this);
     jQuery('#testimonial .signature-text').html( text.data('text') );
