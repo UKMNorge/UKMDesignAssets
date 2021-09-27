c889fc068696da680eec1cdd262f48fc7577afb6
Kim Duong <kimd9740@gmail.com>
2021-09-27 10:14:31 +0200

    add function for filtering

--- js/page/front/testimonials.js
+++ js/page/front/testimonials.js
@@ -1,34 +1,38 @@
-var text;
+// var text;
 
-jQuery(document).on('mouseover click', '.testimonial-small', function(e) {
-    var bilde = "url('" + jQuery(this).data('img') + "') no-repeat center/cover";
-    jQuery('#testimonial .side-pic').css("background", bilde);
+// jQuery(document).on('click', '.testimonial-small', function(e) {
 
-    $('#testimonial .signature-text').animate({left: '250px'}, function(){
-        jQuery('#testimonial .signature-text').html( jQuery(this).data('text') );
-        jQuery('#testimonial .signature').html( jQuery(this).data('signature'));
+//     $('#testimonial .signature-text').animate({left: '250px'}, function(){
+//         jQuery('#testimonial .signature-text').html( jQuery(this).data('text') );
+//     }).delay(1000).fadeTo('slow', 1);
+
+//     text = jQuery(this);
+//     jQuery('#testimonial .signature-text').html( text.data('text') );
+// });
+
+//   // $(document).ready(function(){
+//   //   var interval = 0;
+//   //   (function switchToImage(img) {
+//   //       $(img).click()
+//   //       var images = $('.testimonial-items li .testimonial-small');
+//   //       interval++;
+//   //       if (interval == $('.testimonial-items li .testimonial-small').length) {
+//   //           interval = 0;
+//   //       }
+//   //       setTimeout(function() { 
+//   //         switchToImage(images[interval]) 
+//   //       }, 5000);
+//   //     }) ($('.testimonial-items li')[0]);
+//   //  });
+
+//   var text;
+
+jQuery(document).on('click', '.btn-filter', function(e) {
+
+    $('.hei').animate({left: '250px'}, function(){
+        jQuery('.hei').html( jQuery(this).data('target') );
     }).delay(1000).fadeTo('slow', 1);
 
-    if (text != jQuery(this).data('text')) {
-        jQuery(this).css({opacity : "1"});
-        // jQuery('.testimonial-small:not(:hover)').css({opacity : "0.5"});
-    }
     text = jQuery(this);
-    jQuery('#testimonial .signature-text').html( text.data('text') );
-    jQuery('#testimonial .signature').html( jQuery(this).data('signature'));
+    jQuery('.hei').html( text.data('target') );
 });
-
-  $(document).ready(function(){
-    var interval = 0;
-    (function switchToImage(img) {
-        $(img).click()
-        var images = $('.testimonial-items li .testimonial-small');
-        interval++;
-        if (interval == $('.testimonial-items li .testimonial-small').length) {
-            interval = 0;
-        }
-        setTimeout(function() { 
-          switchToImage(images[interval]) 
-        }, 5000);
-      }) ($('.testimonial-items li')[0]);
-   });
